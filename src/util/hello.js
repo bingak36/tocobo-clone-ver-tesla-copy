2a4349893c43181cf9c6631afaefd79eef21a35b
lizb <loo4love90@gmail.com>
2026-03-18 19:51:24 +0900

    header hero cta hello update

--- src/util/hello.js
+++ src/util/hello.js
@@ -11,7 +11,7 @@ export const helloData = {
   eyebrow: "Experience the Future with Tesla",
   title: "미래를 향한 <br/>가장 직관적인 움직임",
   description:
-    "기술은 인간의 감각을 확장하고 여정을 자유롭게 해야 합니다. Tesla의 지능형 시스템이 선사하는 여유로움 속에서, 오직 드라이빙의 즐거움과 풍경에만 집중하는 시간을 가지세요.",
+    "기술은 인간의 감각을 확장하고 여정을 자유롭게 해야 합니다. <br/>Tesla의 지능형 시스템이 선사하는 여유로움 속에서,<br/> 오직 드라이빙의 즐거움과 풍경에만 집중하는 시간을 가지세요.",
   cta: {
     label: "MEET TESLA",
     href: "/about"
